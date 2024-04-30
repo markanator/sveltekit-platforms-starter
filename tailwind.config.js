c4aecbef313079835c866b2a88b71a531d4fee3e
Mark Ambrocio <wowmarkambro@gmail.com>
2024-04-29 22:27:43 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	}
+};
+
+export default config;
